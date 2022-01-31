f9814c025238fa229c7d01581453e0deb1630e09
Mitch Lobbes <mitchlobbes@gmail.com>
2022-01-31 15:00:11 +0100

    Added email validation

--- Main.py
+++ Main.py
@@ -1,17 +1,20 @@
 import pandas as pd
+from validate_email import validate_email
 
 
 class TextAnalyser:
 
     def __init__(self, file):
-        self._file = self._read_input(input_file=file)
+        self._df = self._read_input(input_file=file)
+        pd.set_option('display.max_rows', 400)
 
     @staticmethod
     def _read_input(input_file):
         return pd.read_csv(input_file, sep=';', encoding='ISO-8859-1')
 
     def run(self):
-        print(self._file)
+        self._df['is_valid_email'] = self._df['LOGIN'].apply(lambda x: validate_email(x))
+        print(self._df)
 
 
 filename = "text.csv"

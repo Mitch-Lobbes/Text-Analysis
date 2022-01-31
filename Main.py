c8a96521f9e4434362cb4e88d138a8134d603a2d
Mitch Lobbes <mitchlobbes@gmail.com>
2022-01-31 14:49:10 +0100

    Created TextAnalyzer Class basic design

--- Main.py
+++ Main.py
@@ -1,1 +1,19 @@
-print("Hello World")
+import pandas as pd
+
+
+class TextAnalyser:
+
+    def __init__(self, file):
+        self._file = self._read_input(input_file=file)
+
+    @staticmethod
+    def _read_input(input_file):
+        return pd.read_csv(input_file, sep=';', encoding='ISO-8859-1')
+
+    def run(self):
+        print(self._file)
+
+
+filename = "text.csv"
+analysis = TextAnalyser(file=filename)
+analysis.run()
